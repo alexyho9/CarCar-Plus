cbdd74875d58eb47873796eca971e7c9d624272d
Calvin <calvinmach5@gmail.com>
2023-09-06 09:48:04 -0700

    Completed the models.py for service_rest.

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=17)
+    sold = models.BooleanField()
+    import_href = models.CharField(max_length=100, null=True)
+
+
+class Technician(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    employee_id = models.CharField(max_length=50)
+
+
+class Appointment(models.Model):
+    date_time = models.DateTimeField()
+    reason = models.CharField(max_length=100)
+    status = models.CharField(max_length=100)
+    customer = models.CharField(max_length=200)
+    technician = models.ForeignKey(
+        Technician,
+        related_name="appointments",
+        on_delete=models.CASCADE
+    )
