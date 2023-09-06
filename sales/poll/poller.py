145fc95aafac5b2dddbdca3bf360816f428a54f5
Alexander Ho <alexyho9@gmail.com>
2023-09-05 22:34:29 -0700

    Created initial sales data models

--- sales/poll/poller.py
+++ sales/poll/poller.py
@@ -9,8 +9,11 @@ sys.path.append("")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sales_project.settings")
 django.setup()
 
-# Import models from sales_rest, here.
-# from sales_rest.models import Something
+from sales_rest.models import AutomobileVO
+
+
+def get_automobiles():
+    response = requests.get('http://')
 
 
 def poll(repeat=True):
@@ -23,7 +26,7 @@ def poll(repeat=True):
             pass
         except Exception as e:
             print(e, file=sys.stderr)
-        
+
         if (not repeat):
             break
 
