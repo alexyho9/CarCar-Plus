be733bc23ab1d4975a328ef5fbac7d4a38ab6c27
Calvin <calvinmach5@gmail.com>
2023-09-07 09:20:37 -0700

    Added some code for setup

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -9,6 +9,12 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="services">
+            <Route index element={<TechniciansList />} />
+            <Route path="new" element={<TechnicianForm />} />
+            <Route index element={<AppointmentsList />} />
+            <Route path="new" element={<AppointmentForm />} />
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
