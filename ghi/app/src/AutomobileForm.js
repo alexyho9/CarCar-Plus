303ba1192a014563dc21c56e1430e551a0daa570
Alexander Ho <alexyho9@gmail.com>
2023-09-08 17:34:13 -0700

    Added AutomobileList page

--- ghi/app/src/AutomobileForm.js
+++ ghi/app/src/AutomobileForm.js
@@ -2,7 +2,120 @@ import React, {useEffect, useState} from 'react';
 
 
 const AutomobileForm = () => {
+  const [color, setColor] = useState('');
+  const [year, setYear] = useState('');
+  const [vin, setVin] = useState('');
+  const [model, setModel] = useState('');
+  const [models, setModels] = useState([]);
 
+  const handleColorChange = (event) => {
+    setColor(event.target.value);
+  }
+
+  const handleYearChange = (event) => {
+    setYear(event.target.value);
+  }
+
+  const handleVinChange = (event) => {
+    setVin(event.target.value);
+  }
+
+  const handleModelChange = (event) => {
+    setModel(event.target.value);
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = {};
+    data.color = color;
+    data.year = year;
+    data.vin = vin;
+    data.model_id = model;
+
+    const url = 'http://localhost:8100/api/automobiles/';
+    const fetchConfig = {
+      method: "post",
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const response = await fetch(url, fetchConfig);
+    if (response.ok) {
+      setColor('');
+      setYear('');
+      setVin('');
+      setModel('');
+    }
+
+  }
+
+  const fetchData = async () => {
+    let url = 'http://localhost:8100/api/models/';
+    let response = await fetch(url);
+    if (response.ok) {
+      let data = await response.json();
+      setModels(data.models);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+
+    <div className="row">
+      <div className="offset-3 col-6">
+        <div className="shadow p-4 mt-4">
+        <h1>Add a New Automobile to Inventory</h1>
+        <form onSubmit={handleSubmit} id="create-automobile-form">
+
+          <div className="form-floating mb-3">
+            <input onChange={handleColorChange} placeholder="Color" required
+              type="text" id="color" value={color}
+              name="color" className="form-control" />
+            <label htmlFor="color">Color</label>
+          </div>
+
+          <div className="form-floating mb-3">
+            <input onChange={handleYearChange} placeholder="Year" required
+              type="number" id="year" value={year}
+              name="year" className="form-control" />
+            <label htmlFor="year">Year</label>
+          </div>
+
+          <div className="form-floating mb-3">
+            <input onChange={handleVinChange} placeholder="VIN" required
+              type="text" id="vin" value={vin}
+              name="vin" className="form-control" />
+            <label htmlFor="vin">VIN</label>
+          </div>
+
+          <div className="mb-3">
+            <select onChange={handleModelChange} required
+              id="model" name="model" value={model}
+              className="form-select">
+              <option value="">Choose a Manufacturer</option>
+              {models.map(model => {
+                return (
+                  <option value={model.id} key={model.id}>
+                    {model.manufacturer.name} {model.name}
+                  </option>
+                  );
+              })}
+            </select>
+          </div>
+
+          <button className="btn btn-primary">Create</button>
+
+        </form>
+        </div>
+      </div>
+    </div>
+
+  );
 
 }
+
 export default AutomobileForm;
--- ghi/app/src/AutomobileList.js
+++ ghi/app/src/AutomobileList.js
@@ -2,21 +2,48 @@ import React, {useEffect, useState} from 'react';
 
 
 const AutomobileList = () => {
+  const [autos, setAutos] = useState([]);
 
+  const fetchData = async () => {
+    const url = 'http://localhost:8100/api/automobiles/';
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      setAutos(data.autos);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div className="shadow p-4 mt-4">
-      <h1>Models</h1>
+      <h1>Automobiles</h1>
       <table className="table table-striped">
         <thead>
           <tr>
-            <th>Name</th>
+            <th>VIN</th>
+            <th>Color</th>
+            <th>Year</th>
+            <th>Model</th>
             <th>Manufacturer</th>
-            <th>Picture</th>
+            <th>Sold</th>
           </tr>
         </thead>
         <tbody>
-
+          {autos.map(auto => {
+            return (
+              <tr key={auto.vin}>
+                <td>{auto.vin}</td>
+                <td>{auto.color}</td>
+                <td>{auto.year}</td>
+                <td>{auto.model.name}</td>
+                <td>{auto.model.manufacturer.name}</td>
+                <td>{auto.sold ? "Yes" : "No"}</td>
+              </tr>
+            )
+          })}
         </tbody>
       </table>
     </div>
